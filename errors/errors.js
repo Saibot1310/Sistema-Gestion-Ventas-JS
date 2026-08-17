189394bb17cb4698452362a9633e1b9b85552d00
Tobias Vega <tobivega1310@gmail.com>
2026-08-17 12:36:56 -0300

    refactor: formalizar jerarquía de errores

--- errors/errors.js
+++ errors/errors.js
@@ -1,25 +1,29 @@
-export class ProductoNoEncontradoError extends Error {
+export class ErrorDominio extends Error {
+  constructor(mensaje) {
+    super(mensaje);
+    this.name = this.constructor.name;
+  }
+}
+
+export class ProductoNoEncontradoError extends ErrorDominio {
   constructor(nombreProducto) {
     super(`No se encontró ningún producto con el nombre ${nombreProducto}`);
-    this.name = 'ProductoNoEncontradorError';
     this.nombreProducto = nombreProducto;
   }
 }
 
-export class ProductoInvalidoError extends Error {
+export class ProductoInvalidoError extends ErrorDominio {
   constructor(mensaje, campo) {
     super(mensaje);
-    this.name = 'ProductoInvalidoError';
     this.campo = campo;
   }
 }
 
-export class StockInsuficienteError extends Error {
+export class StockInsuficienteError extends ErrorDominio {
   constructor(nombreProducto, disponible, solicitado) {
     super(
       `Stock insuficiente para ${nombreProducto}: disponible ${disponible}, solicitado: ${solicitado}`,
     );
-    this.name = 'StockInsuficienteError';
     this.disponible = disponible;
     this.solicitado = solicitado;
   }
